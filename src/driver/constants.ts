a50750646cbbf0f99a48d2f108be6d615efd5725
ylc395 <cyl@cyl.moe>
2021-11-10 10:26:52 +0800

    fix: add namespace for script id

--- src/driver/constants.ts
+++ src/driver/constants.ts
@@ -1,4 +1,4 @@
-export const MARKDOWN_SCRIPT_ID = 'contentScriptMarkdownIt';
+export const MARKDOWN_SCRIPT_ID = 'ylc395.ocr.contentScriptMarkdownIt';
 
 export const WINDOW_WIDTH = 600;
 export const WINDOW_HEIGHT = 400;
